bc421846bc51348e2df80234e5dbbafefa1ca54b
Olaf <okappes@amadeus.com>
2012-07-11 10:27:21 +0200

    [fix] this test didn't work in Chrome

--- latest/tests/snippets/templates/TemplateScripts.js
+++ latest/tests/snippets/templates/TemplateScripts.js
@@ -6,7 +6,7 @@ Aria.classDefinition({
 
         testAsyncLoadTplOne : function () {
             document.getElementById("TESTAREA").style.display="none";
-            window.alert = console.log;
+            window.alert = function(m){console.log(m)};
             Aria.loadTemplate({
                 div : "TESTAREA",
                 classpath : "snippets.templates.templateScripts.MyTemplate"
