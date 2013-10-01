70a0e75000c4bf228da1f20ab0ebfaf2217c5a61
Simon Arbuckle <simonarbuckle@hotmail.com>
2013-10-01 10:51:56 +0200

    Creating a new sample to demonstrate the checkbox html widget.

---
+++ samples/widgets/widgetlibs/html/checkbox/BaseCheckBoxTagScript.js
@@ -0,0 +1,10 @@
+Aria.tplScriptDefinition({
+    $classpath : "samples.widgets.widgetlibs.html.checkbox.BaseCheckBoxTagScript",
+    $prototype : {
+        $dataReady : function () {
+            this.data = {
+                selected : false
+            };
+        }
+    }
+});
