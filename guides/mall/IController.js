c2e78e72bcb6f9578460b3d0b95d881caffc437a
okappes <okappes@amadeus.com>
2014-02-17 16:32:59 +0100

    Updated guides archives

--- guides/mall/IController.js
+++
@@ -1,7 +0,0 @@
-Aria.interfaceDefinition({
-    $classpath : "ariadoc.guides.mall.IController",
-    $extends : "aria.templates.IModuleCtrl",
-    $events : {
-        "dataChange" : "Raised when the data model is updated"
-    }
-});
